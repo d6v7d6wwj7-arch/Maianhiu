49e2964cef7b32e17b71dc033c9b6915b757a7c2
dohung239 <d6v7d6wwj7@privaterelay.appleid.com>
2025-11-18 11:19:29 +0700

    Update script.js

--- Galaxy-love-main/script.js
+++ Galaxy-love-main/script.js
@@ -587,10 +587,10 @@ scene.add(planet);
 
 //Ganti Deskripsi Planet
 const ringTexts = [
-  'Galaxy of love From ...',//untuk deskripsi planet layer 1
-  "I love you",//untuk deskripsi planet layer 2
-  "♡Happy Girlfriend day♡",//untuk deskripsi planet layer 3
-  "01/08/2025",//untuk deskripsi planet layer 4
+  'Meeting you is the best thing that ever happened to me',//untuk deskripsi planet layer 1
+  "I love you in every universe",//untuk deskripsi planet layer 2
+  "Today, tomorrow, forever",//untuk deskripsi planet layer 3
+  "08/05/2025",//untuk deskripsi planet layer 4
   ...(window.dataCCD && window.dataCCD.data.ringTexts ? window.dataCCD.data.ringTexts : [])
 ];
 
