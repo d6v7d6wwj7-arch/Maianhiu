e11c17da6d6881e4d554c600a5bcec38bbd51e88
dohung239 <d6v7d6wwj7@privaterelay.appleid.com>
2025-11-18 11:41:56 +0700

    Update script.js

--- Galaxy-love-main/script.js
+++ Galaxy-love-main/script.js
@@ -79,7 +79,7 @@ const galaxyParameters = {
   outsideColor: new THREE.Color(0x48b8b8),
 };
 
-const defaultHeartImages = Array.from({ length: 22 }, (_, i) => `images/img${i + 1}.jpg`);
+const defaultHeartImages = Array.from({ length: 15 }, (_, i) => `images/img${i + 1}.jpg`);
 
 const heartImages = [
   ...(window.dataCCD?.data?.heartImages || []),
